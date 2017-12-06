b6c114364b8768103070b1d9be6d19830247747c
Jotham (jotham.co) <jotham.read@gmail.com>
2017-12-06 14:18:57 +1300

    Fixed up parser

--- namecheap.py
+++ namecheap.py
@@ -3,21 +3,21 @@ from __future__ import print_function
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
+BASE_URL = 'https://www.namecheap.com/domains/registration/results.aspx?domain='
+
 def find_domains(domainsearch):
     driver = webdriver.PhantomJS()
-    driver.get('https://www.namecheap.com/domains/registration/results.aspx?domain='
-            + domainsearch)
-
+    driver.get(BASE_URL + domainsearch)
     html = driver.page_source
-    soup = BeautifulSoup(html)
-
-    domains = ""
+    soup = BeautifulSoup(html, 'html.parser')
+    domains = []
     for li in soup.find_all('li', {"class": "register"}):
         domain = li.find('span', {"class": "domain-name"})
         price = li.find('span', {"class": "domain-dollar-value"})
-        domains += domain.text + ' ' +  price.text.replace('year', 'yr') + ', '
+        if domain and price:
+           domains.append(domain.text + ' ' +  price.text.replace('year', 'yr'))
     return domains
 
 if __name__ == "__main__":
     domains = find_domains('plzbum.lol')
-    print(domains)
+    print(", ".join(domains))
